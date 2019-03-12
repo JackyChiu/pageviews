d2d16e78907aae36247001d6f0f0e9fc008b5727
vikrambombhi <vbombhi@gmail.com>
2019-03-12 17:21:45 -0400

    reading file and accumulating views by url

--- lib/file_reader.ex
+++ lib/file_reader.ex
@@ -1,5 +1,24 @@
 defmodule Pageviews.FileReader do
   def run do
     IO.puts("hello")
+
+    streams =
+      for _ <- File.ls!("./") do
+        File.stream!("./pageviews-20190312-180000", read_ahead: 100_000)
+      end
+
+    streams
+    |> Flow.from_enumerables()
+    |> Flow.map(&String.split(&1, " "))
+    |> Flow.map(fn line ->
+      {views, _} = Enum.at(line, 2) |> Integer.parse()
+      {Enum.at(line, 1), views}
+    end)
+    |> Flow.partition()
+    |> Flow.reduce(fn -> %{} end, fn {url, views}, acc ->
+      Map.update(acc, url, 1, &(&1 + views))
+    end)
+    |> Enum.to_list()
+    |> IO.inspect()
   end
 end
