086bcbf79d9ed29f8ad34b217442672bcbd1aef6
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-05-21 14:59:10 -0400

    simplify topviews insertion

--- lib/pageviews/topviews.ex
+++ lib/pageviews/topviews.ex
@@ -7,24 +7,27 @@ defmodule Pageviews.Topviews do
 
   def add_line(pid, {page, views}) do
     Agent.update(pid, fn topviews ->
-      with true <- length(topviews) >= 25,
-           [{_, lowest_topview} | tail] <- topviews do
-        if lowest_topview < views do
-          [{page, views} | tail]
-          |> Enum.sort(&compare/2)
-        else
-          topviews
-        end
-      else
-        _ ->
-          [{page, views} | topviews]
-          |> Enum.sort(&compare/2)
-      end
+      insert({page, views}, topviews)
     end)
   end
 
   def get_top(pid) do
     Agent.get(pid, & &1)
+  end
+
+  defp insert({page, views}, topviews) when length(topviews) < 25 do
+    [{page, views} | topviews]
+    |> Enum.sort(&compare/2)
+  end
+
+  defp insert({page, views}, topviews) do
+    with [{_, lowest_view} | tail] <- topviews,
+         true <- lowest_view < views do
+      [{page, views} | tail]
+      |> Enum.sort(&compare/2)
+    else
+      _ -> topviews
+    end
   end
 
   defp compare({_, a_views}, {_, b_views}) do
