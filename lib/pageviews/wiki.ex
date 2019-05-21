32ab63464784c717b9d3ab5b2a3c4278db478731
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-05-21 14:58:57 -0400

    Simplify logic into an emit function

--- lib/pageviews/wiki.ex
+++ lib/pageviews/wiki.ex
@@ -19,56 +19,49 @@ defmodule Pageviews.Wiki do
     {:producer, {zstream, [], 0, false}}
   end
 
+  # Handles demand for the pageview lines requested by the consumer.
   def handle_demand(demand, {zstream, lines, remaining_demand, done}) do
-    remaining_demand = remaining_demand + demand
-    {events, lines} = Enum.split(lines, remaining_demand)
-
-    cond do
-      done and length(lines) == 0 ->
-        IO.puts("GenStage dieing #{length(lines)}")
-        {:stop, :shutdown, {zstream, lines, remaining_demand, done}}
-
-      true ->
-        remaining_demand = remaining_demand - length(events)
-        # IO.puts("EMITTING #{length(events)} events")
-        {:noreply, events, {zstream, lines, remaining_demand, done}}
-    end
+    emit({zstream, lines, remaining_demand + demand, done})
   end
 
+  # Handles chunks of data streaming in from the HTTP request.
   def handle_info(%HTTPoison.AsyncChunk{chunk: chunk}, {zstream, lines, remaining_demand, _done}) do
-    {new_lines, zstream} = inflate_chunk(zstream, :zlib.safeInflate(zstream, chunk), [])
-    lines = Enum.concat(lines, new_lines)
-
-    {events, lines} = Enum.split(lines, remaining_demand)
-    remaining_demand = max(remaining_demand - length(events), 0)
-    {:noreply, events, {zstream, lines, remaining_demand, false}}
+    {zstream, new_lines} = inflate_chunk(zstream, :zlib.safeInflate(zstream, chunk))
+    emit({zstream, lines ++ new_lines, remaining_demand, false})
   end
 
+  # Handles the end of the HTTP request's stream.
   def handle_info(%HTTPoison.AsyncEnd{}, {zstream, lines, remaining_demand, _done}) do
-    IO.puts("DONE DOWNLOADING")
+    IO.puts("HTTP REQUEST DONE STREAMING")
     :zlib.inflateEnd(zstream)
     :zlib.close(zstream)
 
-    {events, lines} = Enum.split(lines, remaining_demand)
-    remaining_demand = remaining_demand - length(events)
-
-    cond do
-      length(events) > 0 ->
-        {:noreply, events, {zstream, lines, remaining_demand, true}}
-
-      true ->
-        IO.puts("GenStage dieing #{length(lines)}")
-        {:stop, :shutdown, {zstream, lines, remaining_demand, true}}
-    end
+    emit({zstream, lines, remaining_demand, true})
   end
 
   def handle_info(_msg, state) do
     {:noreply, [], state}
   end
 
+  # Emits as much events as the remaining_demand requests for.
+  # Stops once the stream is finished and there are no more leftover
+  # events.
+  defp emit({zstream, lines, remaining_demand, done}) do
+    {lines, leftover} = Enum.split(lines, remaining_demand)
+    remaining_demand = remaining_demand - length(lines)
+
+    cond do
+      done and length(leftover) == 0 ->
+        {:stop, :shutdown, {zstream, leftover, remaining_demand, done}}
+
+      true ->
+        {:noreply, lines, {zstream, leftover, remaining_demand, done}}
+    end
+  end
+
+  # Inflates the chunk of data incrementally.
   defp inflate_chunk(zstream, chunk, acc_lines \\ [])
 
-  # Inflates the chunk of data incrementally until there is no more data.
   defp inflate_chunk(zstream, {:continue, raw_lines}, acc_lines) do
     lines = read_lines(raw_lines)
     inflate_chunk(zstream, :zlib.safeInflate(zstream, []), acc_lines ++ lines)
